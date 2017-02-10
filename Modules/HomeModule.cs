ab6940fa006747be23e4c929ae9f88026bb2f4a3
Brandon Cooper <bcooper085@gmail.com>
2017-02-10 11:54:22 -0800

    add dictionary for Post route to send contact id with new address

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -15,6 +15,24 @@ namespace Address
         Contact newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-phone"]);
         return View["contact_added.cshtml", newContact];
       };
+      Get["{id}/contact_info"] = parameter => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Contact selectedContact = Contact.Find(parameter.id);
+
+        List<Address> contactAddress = selectedContact.GetStreet().GetCity().GetZip().GetState();
+
+        string streetAddress = Request.Form["address-street"];
+        string cityAddress = Request.Form["address-city"];
+        string zipAddress = Request.Form["address-zip"];
+        string stateAddress = Request.Form["address-state"];
+        Address newAddress = new Address(streetAddress, cityAddress, zipAddress, stateAddress);
+
+        contactAddress.Add(newAddress);
+
+        model.Add("contact", selectedContact);
+        model.Add("address", contactAddress);
+        return View["contact_info", model];
+      };
     }
   }
 }
