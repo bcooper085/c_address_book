a52a84c85ed20d7f72c14bf92be9719d3efd3195
Brandon Cooper <bcooper085@gmail.com>
2017-02-10 09:01:59 -0800

    contact class created

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -4,6 +4,43 @@ namespace Contact.Objects
 {
   public class Contact
   {
-    
+    private string _name;
+    private string _phone;
+    private string _address;
+
+    public Contact (string name, string phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
   }
 }
